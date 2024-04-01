5704aeb36337034dec9897edee79c6cae2cfc1a5
asametduran <97441968+asametduran@users.noreply.github.com>
2024-04-01 10:49:37 +0300

    Add files via upload

---
+++ Algoritma Programlama ve Veri Yapılarına Giriş/karakterDizileri/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int sayi = 2;
+
+        Console.WriteLine(sayi); //2
+        Console.WriteLine(sayi + 2); //4
+                                     // açıklama ifadesi Console.WriteLine(sayi*sayi);
+                                     //açıklama notları Console.WriteLine(sayi-5);
+        /* Bu şekilde de yorum satırı yapabilirsin */
+
+        //Kaçış ifadeleri
+
+        string ifade = "C:\\user\\zcomrt";
+        Console.WriteLine(ifade);
+        // \n yeni satirda baslar.
+        // \t tab yapar.
+        //n \a ses cikarip uyari verir.
+        // eger dosya yolu gibi işlemlerde \ kullanacaksan iki kere kullanip yazmalisin ornek: "C:\\user\\zcomrt"
+        // veya basina @ koymalisin.
+
+
+
+        Console.ReadKey();
+    }
+}
---
+++ Sıfırdan İleri Seviye Python Programlama/variables.py
@@ -0,0 +1,45 @@
+maasAli = 5000
+maasAhmet = 4000
+vergi = 0.27
+
+print(maasAli - (maasAli*vergi))
+print(maasAhmet - (maasAhmet*vergi))
+
+# Değişken tanımlama kuralları
+
+# rakam ile başlayamaz
+
+number1 = 10
+print(number1)
+number1 = 20
+print(number1)
+number1 += 30
+print(number1)
+
+#Büyük küöük hharf duyarlılığı 
+
+age = 20
+AGE = 30 
+print(age)
+print(AGE)
+
+# Türkçe karakter kullanmayalım 
+
+yaş = 20
+_age = 20 
+
+x = 1 #int 
+y = 2.3  #float
+name= "Çınar" #String
+isStudent = True #bool
+
+a='10'
+b='20'
+print(a+b)                #30  => 1020
+
+firstName = "Sadık"
+lastName = " turan "
+
+print(firstName + lastName)
+
+#x, y, name, isStudent  = (1,2,"Sadık",bool)
